34544f10f9d587dcc38f9feba7e5058af2d18cf5
Danieltacorich <89612438+Danieltacorich@users.noreply.github.com>
2022-09-17 02:36:48 +0000

    Sep 16, 2022, 10:36 PM

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -3,18 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
-
 {
     public GameObject[] waypoints;
     int current = 0;
     float rotspeed;
     public float speed;
     float WPradius = 1;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
