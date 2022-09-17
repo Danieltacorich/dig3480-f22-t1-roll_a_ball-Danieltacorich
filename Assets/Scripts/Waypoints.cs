03c07e8b4ea57bf38c415142b894b57801f0e40c
Danieltacorich <89612438+Danieltacorich@users.noreply.github.com>
2022-09-17 19:38:33 +0000

    Sep 17, 2022, 3:38 PM

--- Assets/Scripts/Waypoints.cs
+++ Assets/Scripts/Waypoints.cs
@@ -4,23 +4,38 @@ using UnityEngine;
 
 public class Waypoints : MonoBehaviour  
 {
-    public GameObject[] waypoints;
-    int current = 0;
-    float rotSpeed;
-    public float speed;
-    float WPradius = 1;
+    // Transforms to act as start and end markers for the journey.
+    public Transform startMarker;
+    public Transform endMarker;
 
-    // Update is called once per frame
+    // Movement speed in units/sec.
+    public float speed = 1.0F;
+
+    // Time when the movement started.
+    private float startTime;
+
+    // Total distance between the markers.
+    private float journeyLength;
+
+    void Start()
+    {
+        // Keep a note of the time the movement started.
+        startTime = Time.time;
+
+        // Calculate the journey length.
+        journeyLength = Vector3.Distance(startMarker.position, endMarker.position);
+    }
+
+    // Follows the target position like with a spring
     void Update()
     {
-        if (Vector3.Distance(waypoints[current].transform.position, Transform.position) < WPradius)
-        {
-            current++;
-            if (current >= waypoints.Length)
-            {
-                Current = 0;
-            }
-            transform.position = Vector3.MoveTowards(Transform.position, waypoints[current].transform.position, Time.deltaTime * speed);
-        }
+        // Distance moved = time * speed.
+        float distCovered = (Time.time - startTime) * speed;
+
+        // Fraction of journey completed = current distance divided by total distance.
+        float fracJourney = distCovered / journeyLength;
+
+        // Set our position as a fraction of the distance between the markers and pingpong the movement.
+        transform.position = Vector3.Lerp(startMarker.position, endMarker.position, Mathf.PingPong(fracJourney, 1));
     }
 }
