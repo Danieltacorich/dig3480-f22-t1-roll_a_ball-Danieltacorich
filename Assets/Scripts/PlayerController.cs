c73b4a3554da872e4b1a59d6841ce17c3f553d77
Danieltacorich <89612438+Danieltacorich@users.noreply.github.com>
2022-09-18 22:13:19 +0000

    Sep 18, 2022, 6:13 PM

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -98,7 +98,7 @@ public class PlayerController : MonoBehaviour
         if (lives == 0)
         {
             loseTextObject.SetActive(true);
-            Destroy (gameObject);
+            //Destroy (gameObject);
         }
     }
 
